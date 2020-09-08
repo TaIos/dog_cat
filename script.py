d132b2299a68ab50596366da6f4e58d7d285bd6a
Martin Šafránek <martin.safranek@itelligence.cz>
2020-09-08 13:38:42 +0200

    Add dog functionality

--- script.py
+++ script.py
@@ -3,8 +3,14 @@ import sys
 def default():
     print("Ahoj")
 
+def dog():
+    print("Haf")
+
 def main():
-    default()
+    if sys.argv[1] == "dog":
+        dog()
+    else:
+        default()
 
 if __name__ == "__main__":
     main()
