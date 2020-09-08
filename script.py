0787d30793d3e9f3d11fe84b11d23b3acdc2d8f0
Martin Šafránek <martin.safranek@itelligence.cz>
2020-09-08 13:42:03 +0200

    Add cat functionality

--- script.py
+++ script.py
@@ -3,8 +3,14 @@ import sys
 def default():
     print("Ahoj")
 
+def cat():
+    print("Mnau")
+
 def main():
-    default()
+    if sys.argv[1] == "cat":
+        cat()
+    else:
+        default()
 
 if __name__ == "__main__":
     main()
